ed3386aa456664e70e8d51dbfa1adb3aa060a090
世彬 洪 <touycvbn95147@gmail.com>
2022-04-22 20:32:22 +0800

    add interview questions

---
+++ LeetCode_CSharp/Problems/Interview_Base7Converter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_CSharp.Problems
+{
+    internal class Interview_Base7Converter
+    {
+        private const int baseNum = 7;
+        public string Convert(int num)
+        {
+            var isNegative = num < 0;
+
+            var result = string.Empty;
+            var absNum = Math.Abs(num);
+
+            while (absNum >= baseNum)
+            {
+                var remainder = absNum % baseNum;
+                result = remainder + result;
+
+                absNum /= baseNum;
+            }
+
+            result = absNum + result;
+
+            return (isNegative ? "-" : string.Empty) + result;
+        }
+    }
+}
--- LeetCode_CSharp/Program.cs
+++ LeetCode_CSharp/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace LeetCode_CSharp
+﻿using System.Collections.Generic;
+using LeetCode_CSharp.Problems;
+
+namespace LeetCode_CSharp
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+
         }
     }
 }
