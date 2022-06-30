1addc5c3c514e12847538a33be5e30e79e1fd189
世彬 洪 <touycvbn95147@gmail.com>
2022-06-30 22:31:03 +0800

    Add Q78

---
+++ LeetCode_CSharp/Problems/Q78_Subsets.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode_CSharp.Problems
+{
+    public class Q78_Subsets
+    {
+        public IList<IList<int>> Subsets(int[] nums)
+        {
+            var result = new List<IList<int>>() {new List<int>()};
+
+            foreach (var num in nums)
+            {
+                var resultCount = result.Count;
+
+                for (var index = 0; index < resultCount; index++)
+                {
+                    var newList = result[index].ToList();
+                    newList.Add(num);
+
+                    result.Add(newList);
+                }
+            }
+
+            return result;
+        }
+    }
+}
