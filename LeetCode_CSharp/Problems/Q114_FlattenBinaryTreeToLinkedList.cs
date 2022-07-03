03821098e8f7443c3d7eb7bcc97e6d8c12c37c5a
世彬 洪 <touycvbn95147@gmail.com>
2022-07-03 23:19:03 +0800

    Add Q114

---
+++ LeetCode_CSharp/Problems/Q114_FlattenBinaryTreeToLinkedList.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode_CSharp.Structure;
+
+namespace LeetCode_CSharp.Problems
+{
+    public class Q114_FlattenBinaryTreeToLinkedList
+    {
+        public void Flatten(TreeNode root)
+        {
+            if (root == null) return;
+
+            Flatten(root.Left);
+            Flatten(root.Right);
+
+            var leftFlattenTree = root.Left;
+            var rightFlattenTree = root.Right;
+
+            root.Left = null;
+
+            if (leftFlattenTree != null)
+            {
+                root.Right = leftFlattenTree;
+
+                var lastNode = leftFlattenTree;
+                while (lastNode.Right != null)
+                    lastNode = lastNode.Right;
+
+                lastNode.Right = rightFlattenTree;
+            }
+        }
+    }
+}
