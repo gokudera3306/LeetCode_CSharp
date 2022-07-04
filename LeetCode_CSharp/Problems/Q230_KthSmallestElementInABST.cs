decf74ae195cd989c01c2749efbaeb178273943b
世彬 洪 <touycvbn95147@gmail.com>
2022-07-04 22:13:32 +0800

    Q230

---
+++ LeetCode_CSharp/Problems/Q230_KthSmallestElementInABST.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode_CSharp.Structure;
+
+namespace LeetCode_CSharp.Problems
+{
+    internal class Q230_KthSmallestElementInABST
+    {
+        public int KthSmallest(TreeNode root, int k)
+        {
+            var currentIndex = 0;
+            return TraverseTree(root, k, ref currentIndex);
+        }
+
+        private int TraverseTree(TreeNode node, int k, ref int currentIndex)
+        {
+            if (node == null) return -1;
+
+            var result = TraverseTree(node.Left, k, ref currentIndex);
+            if (result != -1) return result;
+
+            currentIndex++;
+            if (currentIndex == k) return node.Val;
+            
+            result = TraverseTree(node.Right, k, ref currentIndex);
+            if (result != -1) return result;
+
+            return -1;
+        }
+    }
+}
