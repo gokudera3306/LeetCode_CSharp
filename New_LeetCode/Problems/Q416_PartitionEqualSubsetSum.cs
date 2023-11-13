dcbd2dbc6d5e64c398813d417f3420fbe160246a
AdamHung <touycvbn95147@gmail.com>
2023-11-14 02:57:38 +0800

    Add Q416, Q438

---
+++ New_LeetCode/Problems/Q416_PartitionEqualSubsetSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_LeetCode.Problems
+{
+    internal class Q416_PartitionEqualSubsetSum
+    {
+        private Dictionary<(int, int), bool> dp;
+
+        public bool CanPartition(int[] nums)
+        {
+            if (nums.Length == 1) return false;
+
+            var sum = nums.Sum();
+
+            if (sum % 2 != 0) return false;
+
+            var half = sum / 2;
+
+            dp = new Dictionary<(int, int), bool>();
+
+            return BackTracing(nums.ToList(), half);
+        }
+
+        private bool BackTracing(List<int> nums, int target)
+        {
+            if (target == 0) return true;
+            if (target < 0) return false;
+            if (!nums.Any()) return false;
+
+            if (dp.TryGetValue((nums.Count, target), out var value)) return value;
+
+            var current = nums.First();
+            var newList = nums.Skip(1).ToList();
+
+            Console.WriteLine($"Target: {target}, nums: {string.Join(", ", nums)}");
+
+            var result = BackTracing(newList, target - current) || BackTracing(newList, target);
+
+            dp[(nums.Count, target)] = result;
+
+            return result;
+        }
+    }
+}
---
+++ New_LeetCode/Problems/Q438_FindAllAnagramsInString.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_LeetCode.Problems
+{
+    internal class Q438_FindAllAnagramsInString
+    {
+        public IList<int> FindAnagrams(string s, string p)
+        {
+            if (s.Length < p.Length) return new List<int>();
+
+            var ans = new List<int>();
+
+            var windowSize = p.Length;
+            var pDic = new Dictionary<char, int>();
+
+            foreach (var c in p)
+            {
+                if (pDic.ContainsKey(c))
+                    pDic[c]++;
+                else
+                    pDic[c] = 1;
+            }
+
+            for (var i = 0; i <= s.Length - windowSize; i++)
+            {
+                var isAnagram = true;
+                var tempDic = new Dictionary<char, int>();
+
+                for (var j = 0; j < windowSize; j++)
+                {
+                    var currentChar = s[i + j];
+
+                    if (!pDic.ContainsKey(currentChar))
+                    {
+                        i += j;
+                        isAnagram = false;
+                        break;
+                    }
+
+                    if (tempDic.ContainsKey(currentChar))
+                        tempDic[currentChar]++;
+                    else
+                        tempDic[currentChar] = 1;
+
+                    if (tempDic[currentChar] > pDic[currentChar])
+                    {
+                        isAnagram = false;
+                        break;
+                    }
+                }
+
+                if (isAnagram)
+                    ans.Add(i);
+            }
+
+            return ans;
+        }
+
+        //TODO: Better solution
+        /*
+         *public class Solution {
+       public IList<int> FindAnagrams(string s, string p)
+        {
+            if (p.Length > s.Length)
+                return new List<int>();
+            List<int> result = new List<int>();
+            int[] mapP = new int[26];
+            foreach (var symbol in p)
+                mapP[symbol - 'a']++;
+            for (int i = 0; i < p.Length; i++)
+                mapP[s[i] - 'a']--;
+            for (int i = p.Length; i < s.Length; i++)
+            {
+                if (IsAnagram(mapP))
+                    result.Add(i - p.Length);
+                mapP[s[i - p.Length] - 'a']++;
+                mapP[s[i] - 'a']--;
+            }
+            if (IsAnagram(mapP))
+                result.Add(s.Length - p.Length);
+            return result;
+        }
+        bool IsAnagram(int[] map)
+        {
+            for (int i = 0; i < 26; i++)
+                if (map[i] != 0)
+                    return false;
+            return true;
+        }
+}
+         */
+    }
+}
