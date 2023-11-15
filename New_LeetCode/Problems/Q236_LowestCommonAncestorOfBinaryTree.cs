80e4c410d0b3dc974b99948199ac98a761e34a1a
AdamHung <touycvbn95147@gmail.com>
2023-11-15 12:51:15 +0800

    add Q236

---
+++ New_LeetCode/Problems/Q236_LowestCommonAncestorOfBinaryTree.cs
@@ -0,0 +1,17 @@
+﻿using New_LeetCode.Structure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace New_LeetCode.Problems
+{
+    internal class Q236_LowestCommonAncestorOfBinaryTree
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+
+        }
+    }
+}
