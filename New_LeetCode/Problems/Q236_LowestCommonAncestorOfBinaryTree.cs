311b1a48d107032464e581d827efad75109366d0
adamhung <touycvbn95147@gmail.com>
2023-11-18 16:14:29 +0800

    Complete Q236

--- New_LeetCode/Problems/Q236_LowestCommonAncestorOfBinaryTree.cs
+++ New_LeetCode/Problems/Q236_LowestCommonAncestorOfBinaryTree.cs
@@ -11,7 +11,42 @@ namespace New_LeetCode.Problems
     {
         public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
         {
+            var nodeStack = new Stack<TreeNode>();
 
+            var isFoundCount = 0; 
+            traverse(root, p, q, nodeStack, ref isFoundCount);
+
+            return nodeStack.Pop();
         }
+
+        private void traverse(TreeNode? current, TreeNode treeNode, TreeNode treeNode1, Stack<TreeNode> nodeStack, ref int isFoundCount)
+        {
+            if (isFoundCount == 2) return;
+            if (current == null) return;
+
+            var hasPush = false;
+            
+            if (isFoundCount < 1)
+            {
+                hasPush = true;
+                nodeStack.Push(current);
+                
+                Console.WriteLine("Stack: " + string.Join(',', nodeStack.Select(_ => _.val)));
+            }
+
+            if (current.Equals(treeNode) || current.Equals(treeNode1)) isFoundCount++;
+            if (isFoundCount == 2) return;
+            
+            traverse(current.left, treeNode, treeNode1, nodeStack, ref isFoundCount);
+            if (isFoundCount == 2) return;
+            
+            traverse(current.right, treeNode, treeNode1, nodeStack, ref isFoundCount);
+            if (isFoundCount == 2) return;
+
+            if (hasPush)
+                nodeStack.Pop();
+        }
+        
+        //TODO: Better solution
     }
 }
